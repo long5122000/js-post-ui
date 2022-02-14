5467f407e0829d9bd80728ac1c06942732f06e8f
longdl <longluong864@gmail.com>
2022-02-14 18:58:54 +0700

    Set up module axios

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+import axiosClient from './api/axiosClient'
+import postApi from './api/postApi';
 console.log('Hello')
+async function  main() {
+    try{
+
+        const queryParams = {
+            _page:1,
+            _limit:5,
+        }
+        const data = await postApi.getAll(queryParams)
+        console.log('main.js data',data);
+    }catch(error){
+        console.log('get all failed', error);
+    }
+}
+main();
